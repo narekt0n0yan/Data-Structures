afc26e63813c0b964dfe1779d0efd8cdc950990c
narek <narektonoyan142@gmail.com>
2025-04-11 20:07:32 +0400

    >>>

--- TreeNode.cpp
+++
@@ -1,6 +0,0 @@
-#include "TreeNode.h"
-
-TreeNode::TreeNode() : val(0), left(nullptr), right(nullptr) {}
-TreeNode::TreeNode(int x) : val(x), left(nullptr), right(nullptr) {} 
-TreeNode::TreeNode(int x, TreeNode* left, TreeNode* right) 
-    : val(x), left(left), right(right) {}
